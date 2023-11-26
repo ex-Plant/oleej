fca985de6c7662dfe89aefec619a753e09113722
ex-Plant <ka@navarra-lab.com>
2023-11-26 19:12:40 +0100

    add links to main navigation

--- src/components/navigation/Header.svelte
+++ src/components/navigation/Header.svelte
@@ -10,17 +10,48 @@
 </script>
 
 <div
-  class=" px-primary fixed left-0 top-0 mx-auto flex h-[120px] w-full  bg-primary"
+  class=" px-primary fixed left-0 top-0 mx-auto flex min-h-[120px] w-full bg-primary"
 >
-  <div class="flex w-full items-center justify-between border-b border-black">
-    <button
-      class="w-full text-left text-24 uppercase"
-      on:click="{() => alert('coming soon')}"
+  <nav
+    class="no-scrollbar flex h-20 w-full items-center justify-between overflow-x-scroll border-b border-black"
+  >
+    <div class="pr-4">
+      <a class="flex h-20 w-[130px]" href="/"> <Logo /></a>
+    </div>
+    <div
+      class="xl:space-x-z flex gap-4 md:gap-x-8 lg:gap-x-12 xl:gap-x-16 [&_button]:uppercase"
     >
-      [ menu ]
-    </button>
-    <a href='/' class=" flex h-full">
-      <Logo />
-    </a>
-  </div>
+      <button on:click="{() => alert('21:37')}" class="group relative">
+        <a href="/">Blog</a>
+        <span
+          class="{'absolute bottom-[-3px] left-[50%] h-[1px] w-0 rounded bg-black duration-300' +
+            ' group-hover:left-0 group-hover:w-full'}"></span>
+      </button>
+
+      <button on:click="{() => alert('21:37')}" class="group relative">
+        <a class="whitespace-nowrap" href="#">O mnie</a>
+        <span
+          class="{'absolute bottom-[-3px] left-[50%] h-[1px] w-0 rounded bg-black duration-300' +
+            ' group-hover:left-0 group-hover:w-full'}"></span>
+      </button>
+      <button on:click="{() => alert('21:37')}" class="group relative">
+        <a href="/">Kontakt</a>
+        <span
+          class="{'absolute bottom-[-3px] left-[50%] h-[1px] w-0 rounded bg-black duration-300' +
+            ' group-hover:left-0 group-hover:w-full'}"></span>
+      </button>
+      <button on:click="{() => alert('21:37')}" class="group relative">
+        <a href="#">Szukaj</a>
+        <span
+          class="{'absolute bottom-[-3px] left-[50%] h-[1px] w-0 rounded bg-black duration-300' +
+            ' group-hover:left-0 group-hover:w-full'}"></span>
+      </button>
+    </div>
+  </nav>
+  <!--    <button-->
+  <!--      class="w-full text-left text-24 uppercase"-->
+  <!--      on:click="{() => alert('coming soon')}"-->
+  <!--    >-->
+  <!--      [ menu ]-->
+  <!--    </button>-->
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,22 +18,52 @@
   }
 </script>
 
-<!--todo - this is completely fucked up logic -> get proper data object with post-->
-
 <nav
-  title='categories navigation'
-  class="fixed top-20 flex items-center space-x-6 overflow-x-scroll [&_button]:uppercase"
+  title="categories navigation"
+  class='fixed top-20 left-0 right-0 px-primary flex items-center space-x-6 overflow-x-scroll no-scrollbar [&_button]:h-10 [&_button]:uppercase  '
 >
-  <button aria-label='reset categories' on:click="{() => filterByCategory('')}">
+  <button
+    class='group relative '
+    aria-label="reset categories"
+    on:click="{() => filterByCategory('')}"
+  >
     <AllCategoriesButton />
-    <span class=" mt-1 block h-[2px] w-[18px] bg-black"></span>
+    <span
+      class={'absolute bottom-0 left-[50%] h-[1px] w-0 rounded bg-black duration-200' +
+        ' group-hover:left-0 group-hover:w-full'}
+    ></span>
   </button>
   <button
-    on:click="{() => filterByCategory('na poważnie')}"
-    class="whitespace-nowrap">na poważnie</button
+    class='group relative'
+    on:click="{() => filterByCategory('na poważnie')}">
+    <span class='whitespace-nowrap'>    na poważnie</span>
+
+    <span
+      class={'absolute bottom-0 left-[50%] h-[1px] w-0 rounded bg-black duration-300' +
+        ' group-hover:left-0 group-hover:w-full'}
+    ></span>
+  </button>
+  <button
+    class='group relative'
+    on:click="{() => filterByCategory('historie')}">
+    <span>historie</span>
+    <span
+      class={'absolute bottom-0 left-[50%] h-[1px] w-0 rounded bg-black duration-300' +
+        ' group-hover:left-0 group-hover:w-full'}
+    ></span>
+    </button
   >
-  <button on:click="{() => filterByCategory('na poważnie')}">historie</button>
-  <button on:click="{() => filterByCategory('polecane')}">polecane</button>
+  <button
+    class='group relative'
+    on:click="{() => filterByCategory('polecane')}"><span>
+    polecane</span>
+    <span
+      class={'absolute bottom-0 left-[50%] h-[1px] w-0 rounded bg-black duration-300' +
+        ' group-hover:left-0 group-hover:w-full'}
+    ></span>
+
+  </button
+  >
 </nav>
 
 <section title="lista artykulow" class="grid pb-12">
