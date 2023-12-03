e39ddedd9f84c8d712ff829da3bb6296d3ec72a0
ex-Plant <ka@navarra-lab.com>
2023-12-03 23:44:34 +0100

    fix border add types fo blog footer

--- src/components/blogPost/FooterPostList.svelte
+++ src/components/blogPost/FooterPostList.svelte
@@ -1,34 +1,45 @@
 <script lang="ts">
-import {spacesToDashes} from "../../helpers/spacesToDashes";
+  import { spacesToDashes } from '../../helpers/spacesToDashes';
+  import type { ImageType, PostType } from "../../types";
 
-export let posts;
-export let fotos;
-
+  export let posts: PostType[];
+  export let fotos: ImageType[];
 </script>
 
-<section
-  class='mx-auto grid max-w-[1440px]  grid-cols-3 gap-x-8 xl:gap-x-16 border-t border-black pt-6 xl:pt-12 px-primary '>
+<section class="px-primary mx-auto max-w-[1440px]">
+  <div
+    class=" grid grid-cols-3 gap-x-8 border-t border-black pt-6 xl:gap-x-16 xl:pt-12"
+  >
     {#each posts as post (post.acf.slug)}
       {#each fotos as foto (foto.id)}
         {#if post.acf.foto_id === foto.id}
           <a class="" href="{`/blog/${spacesToDashes(post.acf.slug)}`}">
-          <article class=" grid pb-12">
-            <div class=''>
-              <h2 class=" text-desktop30 font-[700] line-clamp-2">{post.acf.title}</h2>
-              <h4 class=" text-desktop20 uppercase pb-3">{post.acf.category}</h4>
-            </div>
-            <div class=" ">
-              <div class=''>
-                  <img src="{foto.source_url}" alt=""
-                       class="aspect-square  object-cover h-[clamp(200px,25vw,400px)]  w-full" />
+            <article class=" grid pb-12">
+              <div class="">
+                <h2 class=" line-clamp-2 text-desktop30 font-[700]">
+                  {post.acf.title}
+                </h2>
+                <h4 class=" pb-3 text-desktop20 uppercase">
+                  {post.acf.category}
+                </h4>
               </div>
+              <div class=" ">
+                <div class="">
+                  <img
+                    src="{foto.source_url}"
+                    alt=""
+                    class="aspect-square h-[clamp(200px,25vw,400px)] w-full object-cover"
+                  />
+                </div>
 
-              <p class="mt-6 line-clamp-4 text-desktop20 ">{post.acf.post_description}</p>
-            </div>
-          </article>
+                <p class="mt-6 line-clamp-4 text-desktop20">
+                  {post.acf.post_description}
+                </p>
+              </div>
+            </article>
           </a>
         {/if}
       {/each}
     {/each}
-
+  </div>
 </section>
