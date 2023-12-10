5badfa529c1e57f1ecbe051417d4d404b7a98541
ex-Plant <ka@navarra-lab.com>
2023-12-10 15:07:16 +0100

    failed build tests!

--- src/store/global.ts
+++ src/store/global.ts
@@ -1,2 +1,9 @@
-import { writable } from "svelte/store";
+import { writable } from 'svelte/store';
 export const loading = writable(false);
+
+if (loading) {
+  setTimeout(() => {
+    loading.set(false);
+    alert('trwa to dłużej niż zwykle - sprawdź połączenie internetowe, lub spróbuj przeładować stronę');
+  }, 10000);
+}
