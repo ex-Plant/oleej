b2430811e3a4bc0898557711acb9ad7470279c04
ex-Plant <ka@navarra-lab.com>
2023-12-10 15:22:10 +0100

    failed build tests3

--- src/store/global.ts
+++ src/store/global.ts
@@ -1,9 +1,2 @@
 import { writable } from 'svelte/store';
 export const loading = writable(false);
-
-if (loading) {
-  setTimeout(() => {
-    loading.set(false);
-    alert('trwa to dłużej niż zwykle - sprawdź połączenie internetowe, lub spróbuj przeładować stronę');
-  }, 10000);
-}
