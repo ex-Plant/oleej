2f1c866d38d26489f1e5d0a82a953f7d06b763fc
ex-Plant <ka@navarra-lab.com>
2024-07-20 14:31:04 +0200

    update new backend server address

--- src/constans/constans.ts
+++ src/constans/constans.ts
@@ -1,3 +1,2 @@
-
-export const baseUrl = 'https://srv71890.seohost.com.pl/wordpress/wp-json/wp/v2/'
-export const graphqlUrl = 'https://serwer2304048.home.pl/wordpress/graphql';
+export const baseUrl = 'https://srv71890.seohost.com.pl/wordpress/wp-json/wp/v2/';
+export const graphqlUrl = 'https://srv71890.seohost.com.pl/graphql';
