29047596cc0f8b49a4b78d335ad476e2e4d91c17
ex-Plant <ka@navarra-lab.com>
2024-07-20 15:25:33 +0200

    test fotos not loading

--- src/constans/constans.ts
+++ src/constans/constans.ts
@@ -1,2 +1,6 @@
-export const baseUrl = 'https://srv71890.seohost.com.pl/wp-json/wp/v2/';
-export const graphqlUrl = 'https://srv71890.seohost.com.pl/graphql';
+// export const baseUrl = 'https://srv71890.seohost.com.pl/wp-json/wp/v2/';
+// export const graphqlUrl = 'https://srv71890.seohost.com.pl/graphql';
+
+
+export const baseUrl = 'https://serwer2304048.home.pl/wordpress/wp-json/wp/v2/'
+export const graphqlUrl = 'https://serwer2304048.home.pl/wordpress/graphql';
