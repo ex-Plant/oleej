efbf1d8cfa032a91dfae9eed38ed8241886a2a30
ex-Plant <ka@navarra-lab.com>
2024-07-20 13:27:11 +0200

    move backend to new server

--- src/constans/constans.ts
+++ src/constans/constans.ts
@@ -1,2 +1,3 @@
-export const baseUrl = 'https://serwer2304048.home.pl/wordpress/wp-json/wp/v2/'
+
+export const baseUrl = 'https://srv71890.seohost.com.pl/wordpress/wp-json/wp/v2/'
 export const graphqlUrl = 'https://serwer2304048.home.pl/wordpress/graphql';
